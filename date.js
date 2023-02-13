65590747d1ccecce6864ae63625422b60f552812
Dedu2ka <artemmelentevfei@gmail.com>
2023-02-13 23:46:11 +0500

    gggggg

---
+++ date.js
@@ -0,0 +1,10 @@
+function renderTime(){
+    var t=new Date();
+    var d= t.setDate(t.getDate()-1+1)&&(t.getDate()-1);
+    var m = t.setMonth(t.getMonth()-8+1)&&t.getMonth();
+    var y = t.getFullYear()-2021;
+    var myClock = document.getElementById('clockDisplay');
+    myClock.textContent = y + "year " + m + "month " + d + "day";
+    setTimeout('renderTime()', 1000); 
+}
+renderTime();
