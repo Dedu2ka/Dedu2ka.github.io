e4a3019f3232069812a7c10f19cef75424eb4eaa
Dedu2ka <artemmelentevfei@gmail.com>
2024-08-05 02:39:02 +0500

    yy

--- date.js
+++ date.js
@@ -2,9 +2,9 @@ function renderTime(){
     var t=new Date();
     var d= t.setDate(t.getDate()-1+1)&&(t.getDate()-1);
     var m = t.setMonth(t.getMonth()-8+1)&&t.getMonth();
-    var y = t.getFullYear()-2021;
+    var yy = t.getFullYear()-2021;
     var myClock = document.getElementById('clockDisplay');
-    myClock.textContent = y + "year " + m + "month " + d + "day";
+    myClock.textContent = yy + "year " + m + "month " + d + "day";
     setTimeout('renderTime()', 1000); 
 }
 renderTime();
